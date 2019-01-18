dfa87c661182176116a3fe591dace3e953618e98
Your Name <you@example.com>
2019-01-18 15:42:54 +0800

    添加功能567

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ struct student
 struct student arr[100];
 int index=0;
 
-int main(){
+int main()
+{
     printf("xueshengchengjiguanlixitong\n");
     while(1)
     {
@@ -86,7 +87,7 @@ int main(){
                 int sum =arr[i].yuwen+arr[i].shuxue+arr[i].yingyu;
                 if(sum<180)
                 {
-                    printf("zo fubujigedetongx %s，语文：%d，数学%d，英语%d\n",arr[i].name,arr[i].yuwen,arr[i].shuxue,arr[i].yingyu);
+                    printf("zo fubujigedetongx %s，yuwu：%d，shuxue%d，yingyu%d\n",arr[i].name,arr[i].yuwen,arr[i].shuxue,arr[i].yingyu);
                 }
             }
             char x;
@@ -96,15 +97,60 @@ int main(){
         }
         if (code==5) 
         {
-            
+            if (code==5) 
+            {
+               printf("mykdbjgdxsrx:\n");
+               
+               for(int i ; i < index; i++)
+               {
+                   if(arr[i].yuwen<60 && arr[i].shuxue<60,arr[i].yingyu<60);
+                   printf("skdbjgdxsxmw%s,yuwu：%d，shuxue%d，yingyu%d\n",arr[i].name,arr[i].yuwen,arr[i].shuxue,arr[i].yingyu);
+
+               }
+               
+            }
+            printf("\ndianjihcjx\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
         }
         if (code==6) 
         {
-            
+            if (code == 6)
+        {
+            printf("总分最高的学生为：\n");
+
+            int max = 0;
+            int stuID = 0;
+
+            for(int i = 0; i < index; i++)
+            {
+                int sum = arr[i].yuwen + arr[i].shuxue + arr[i].yingyu;
+                if(max < sum){
+                    max  =  sum;
+                    stuID = i;
+                }
+            }
+            printf("zfzgdxsxmw%s,yuwei：%d，shuxue%d，yingyu%d\n",arr[stuID].name,arr[stuID].yuwen,arr[stuID].shuxue,arr[stuID].yingyu);
+
+            printf("\ndjhcjx\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code==7) 
         {
-            
+            double max=0.0;
+            int stuID=0;
+            for(int i=0;i<index;i++)
+            {
+                if(arr[i].yuwen >= 60 && arr[i].shuxue >= 60 && arr[i].yingyu >= 60){
+                double avg = (arr[i].yuwen + arr[i].shuxue + arr[i].yingyu)/3.0;
+                if(max < avg){
+                max = avg;
+                stuID = i;
+            }
+
         }
         if (code==8) 
         {
@@ -117,4 +163,7 @@ int main(){
 
     return 0;
 
+        }
+        }
+    }
 }
