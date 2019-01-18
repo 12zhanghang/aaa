60f0b07a1f5fe53402f09818f64c7bd1f19fe069
Your Name <you@example.com>
2019-01-18 10:24:07 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,8 +1,66 @@
 #include <stdio.h>
 
-int main(){
+struct student
+{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char name[100];
+};
 
-    printf("hello world!");
+int main(){
+    printf("xueshengchengjiguanlixitong\n");
+    while(1)
+    {
+        printf("1---tian jia xuesheng chengji\n");
+        printf("2---shan chu\n");
+        printf("3---xsxscj\n");
+        printf("4---ye wu1\n");
+        printf("5---yewu2\n");
+        printf("6---yewu3\n");
+        printf("7---yewu4\n");
+        printf("8---tui chu\n");
+        
+        printf("qing xuan ze:\n");
+        int code;
+        scanf("%d",&code);
+        
+        if (code==1) 
+        {
+            
+        }
+        if (code==2) 
+        {
+            
+        }
+        if (code==3) 
+        {
+            
+        }
+        if (code==4) 
+        {
+            
+        }
+        if (code==5) 
+        {
+            
+        }
+        if (code==6) 
+        {
+            
+        }
+        if (code==7) 
+        {
+            
+        }
+        if (code==8) 
+        {
+            printf("tuichu");
+            break;
+        }
+
+    }
+
 
     return 0;
 
